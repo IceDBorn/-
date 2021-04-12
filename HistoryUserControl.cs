0cfd5ed073e94c2477db90b457d3e49e8196ba7e
Olga-Panagiotidou <77547348+Olga-Panagiotidou@users.noreply.github.com>
2021-04-13 00:29:12 +0300

    history

---
+++ HistoryUserControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace filmhub
+{
+    public partial class HistoryUserControl : UserControl
+    {
+        #region Fields
+
+        private readonly Colors _colors = new Colors();
+
+        #endregion
+        
+        public HistoryUserControl()
+        {
+            InitializeComponent();
+            InitializeColors();
+        }
+        
+        private void InitializeColors()
+        {
+            BackColor = _colors.BackgroundColor;
+        }
+    }
+}
--- MainForm.cs
+++ MainForm.cs
@@ -35,7 +35,7 @@ namespace filmhub
         private void UserControlSelector()
         {
             mainPage.Controls.Clear();
-            mainPage.Controls.Add(new FavoritesUserControl());
+            mainPage.Controls.Add(new HistoryUserControl());
         }
         
         
