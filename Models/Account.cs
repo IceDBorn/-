5d58def761edea5c0186e16cc15481aaa62b08d1
CrazyStevenz <spapanatsios@gmail.com>
2021-04-12 23:23:11 +0200

    Add Accounts

---
+++ Models/Account.cs
@@ -0,0 +1,38 @@
+﻿namespace filmhub.Models
+{
+    public class Account
+    {
+        private int id;
+        private string name;
+        private string surnname;
+        private string email;
+        private string birthdate;
+        private bool admin;
+        private bool darkTheme;
+        private string picture;
+        private string createdOn;
+        
+        private static Account accountInstance = null;
+
+        public Account(int id, string name, string surnname, string email, string birthdate, bool admin, bool darkTheme, string picture, string createdOn)
+        {
+            this.id = id;
+            this.name = name;
+            this.surnname = surnname;
+            this.email = email;
+            this.birthdate = birthdate;
+            this.admin = admin;
+            this.darkTheme = darkTheme;
+            this.picture = picture;
+            this.createdOn = createdOn;
+        }
+        
+        public void login() {
+            accountInstance = this;
+        }
+        
+        public static Account getAccountInstance() {
+            return accountInstance;
+        }
+    }
+}
