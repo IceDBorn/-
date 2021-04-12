859010ebe82583f5bf760311d9966021b3ad02d3
CrazyStevenz <spapanatsios@gmail.com>
2021-04-12 23:24:34 +0200

    Add DatabaseController

---
+++ Controllers/DatabaseController.cs
@@ -0,0 +1,17 @@
+﻿using Npgsql;
+
+namespace filmhub
+{
+    public static class DatabaseController
+    {
+        public static NpgsqlConnection getConnection()
+        {
+            const string cs = "Host=localhost;Username=postgres;Password=1234;Database=postgres";
+
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            return con;
+        }
+    }
+}
