bff76550b042ebb601a17b858ece3d5503aa370b
Olga-Panagiotidou <77547348+Olga-Panagiotidou@users.noreply.github.com>
2021-04-13 00:36:41 +0300

    watchlist

--- MainForm.cs
+++ MainForm.cs
@@ -35,7 +35,7 @@ namespace filmhub
         private void UserControlSelector()
         {
             mainPage.Controls.Clear();
-            mainPage.Controls.Add(new HistoryUserControl());
+            mainPage.Controls.Add(new WatchlistUserControl());
         }
         
         
