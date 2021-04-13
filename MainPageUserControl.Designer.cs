026cd9b604e73b7b5170f6a6c329ed9f457ec6da
Olga-Panagiotidou <77547348+Olga-Panagiotidou@users.noreply.github.com>
2021-04-13 22:22:19 +0300

    open movie categories

--- MainPageUserControl.Designer.cs
+++ MainPageUserControl.Designer.cs
@@ -34,31 +34,46 @@ namespace filmhub
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.featuredToday = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.comingSoon = new System.Windows.Forms.Label();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.pictureBox8 = new System.Windows.Forms.PictureBox();
-            this.pictureBox9 = new System.Windows.Forms.PictureBox();
             this.pictureBox10 = new System.Windows.Forms.PictureBox();
             this.pictureBox11 = new System.Windows.Forms.PictureBox();
             this.pictureBox12 = new System.Windows.Forms.PictureBox();
             this.menu = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.categoriesBox = new System.Windows.Forms.PictureBox();
+            this.categoryTitle = new System.Windows.Forms.Label();
+            this.thriller = new System.Windows.Forms.Label();
+            this.comedy = new System.Windows.Forms.Label();
+            this.romance = new System.Windows.Forms.Label();
+            this.crime = new System.Windows.Forms.Label();
+            this.action = new System.Windows.Forms.Label();
+            this.documentary = new System.Windows.Forms.Label();
+            this.horror = new System.Windows.Forms.Label();
+            this.fantasy = new System.Windows.Forms.Label();
+            this.drama = new System.Windows.Forms.Label();
+            this.mystery = new System.Windows.Forms.Label();
+            this.western = new System.Windows.Forms.Label();
+            this.scifi = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox8)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize) (this.pictureBox9)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox11)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox12)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.menu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.categoriesBox)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -88,15 +103,6 @@ namespace filmhub
             this.pictureBox2.Size = new System.Drawing.Size(144, 171);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox3.Location = new System.Drawing.Point(482, 96);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(144, 171);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
@@ -153,15 +159,6 @@ namespace filmhub
             this.pictureBox8.TabIndex = 9;
             this.pictureBox8.TabStop = false;
             // 
-            // pictureBox9
-            // 
-            this.pictureBox9.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox9.Location = new System.Drawing.Point(482, 384);
-            this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(144, 171);
-            this.pictureBox9.TabIndex = 10;
-            this.pictureBox9.TabStop = false;
-            // 
             // pictureBox10
             // 
             this.pictureBox10.BackColor = System.Drawing.SystemColors.ControlDark;
@@ -198,12 +195,197 @@ namespace filmhub
             this.menu.Size = new System.Drawing.Size(46, 46);
             this.menu.TabIndex = 14;
             this.menu.TabStop = false;
+            this.menu.Click += new System.EventHandler(this.menu_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox3.Location = new System.Drawing.Point(482, 96);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(144, 171);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox9.Location = new System.Drawing.Point(482, 384);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(144, 171);
+            this.pictureBox9.TabIndex = 10;
+            this.pictureBox9.TabStop = false;
+            // 
+            // categoriesBox
+            // 
+            this.categoriesBox.Location = new System.Drawing.Point(0, 21);
+            this.categoriesBox.Name = "categoriesBox";
+            this.categoriesBox.Size = new System.Drawing.Size(242, 557);
+            this.categoriesBox.TabIndex = 15;
+            this.categoriesBox.TabStop = false;
+            this.categoriesBox.Visible = false;
+            this.categoriesBox.Click += new System.EventHandler(this.categoriesBox_Click);
+            // 
+            // categoryTitle
+            // 
+            this.categoryTitle.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.categoryTitle.Location = new System.Drawing.Point(3, 25);
+            this.categoryTitle.Name = "categoryTitle";
+            this.categoryTitle.Size = new System.Drawing.Size(188, 42);
+            this.categoryTitle.TabIndex = 16;
+            this.categoryTitle.Text = "Categories";
+            this.categoryTitle.Visible = false;
+            // 
+            // thriller
+            // 
+            this.thriller.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.thriller.ForeColor = System.Drawing.Color.White;
+            this.thriller.Location = new System.Drawing.Point(32, 96);
+            this.thriller.Name = "thriller";
+            this.thriller.Size = new System.Drawing.Size(106, 38);
+            this.thriller.TabIndex = 17;
+            this.thriller.Text = "Thriller";
+            this.thriller.Visible = false;
+            // 
+            // comedy
+            // 
+            this.comedy.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.comedy.ForeColor = System.Drawing.Color.White;
+            this.comedy.Location = new System.Drawing.Point(32, 135);
+            this.comedy.Name = "comedy";
+            this.comedy.Size = new System.Drawing.Size(142, 38);
+            this.comedy.TabIndex = 18;
+            this.comedy.Text = "Comedy\r\n";
+            this.comedy.Visible = false;
+            // 
+            // romance
+            // 
+            this.romance.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.romance.ForeColor = System.Drawing.Color.White;
+            this.romance.Location = new System.Drawing.Point(32, 173);
+            this.romance.Name = "romance";
+            this.romance.Size = new System.Drawing.Size(161, 38);
+            this.romance.TabIndex = 19;
+            this.romance.Text = "Romance\r\n\r\n";
+            this.romance.Visible = false;
+            // 
+            // crime
+            // 
+            this.crime.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.crime.ForeColor = System.Drawing.Color.White;
+            this.crime.Location = new System.Drawing.Point(32, 211);
+            this.crime.Name = "crime";
+            this.crime.Size = new System.Drawing.Size(106, 38);
+            this.crime.TabIndex = 20;
+            this.crime.Text = "Crime";
+            this.crime.Visible = false;
+            // 
+            // action
+            // 
+            this.action.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.action.ForeColor = System.Drawing.Color.White;
+            this.action.Location = new System.Drawing.Point(32, 249);
+            this.action.Name = "action";
+            this.action.Size = new System.Drawing.Size(106, 38);
+            this.action.TabIndex = 21;
+            this.action.Text = "Action";
+            this.action.Visible = false;
+            // 
+            // documentary
+            // 
+            this.documentary.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.documentary.ForeColor = System.Drawing.Color.White;
+            this.documentary.Location = new System.Drawing.Point(32, 287);
+            this.documentary.Name = "documentary";
+            this.documentary.Size = new System.Drawing.Size(161, 38);
+            this.documentary.TabIndex = 22;
+            this.documentary.Text = "Documentary\r\n";
+            this.documentary.Visible = false;
+            // 
+            // horror
+            // 
+            this.horror.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.horror.ForeColor = System.Drawing.Color.White;
+            this.horror.Location = new System.Drawing.Point(32, 325);
+            this.horror.Name = "horror";
+            this.horror.Size = new System.Drawing.Size(106, 38);
+            this.horror.TabIndex = 23;
+            this.horror.Text = "Horror\r\n";
+            this.horror.Visible = false;
+            // 
+            // fantasy
+            // 
+            this.fantasy.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.fantasy.ForeColor = System.Drawing.Color.White;
+            this.fantasy.Location = new System.Drawing.Point(32, 363);
+            this.fantasy.Name = "fantasy";
+            this.fantasy.Size = new System.Drawing.Size(106, 38);
+            this.fantasy.TabIndex = 24;
+            this.fantasy.Text = "Fantasy";
+            this.fantasy.Visible = false;
+            // 
+            // drama
+            // 
+            this.drama.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.drama.ForeColor = System.Drawing.Color.White;
+            this.drama.Location = new System.Drawing.Point(32, 403);
+            this.drama.Name = "drama";
+            this.drama.Size = new System.Drawing.Size(106, 38);
+            this.drama.TabIndex = 25;
+            this.drama.Text = "Drama\r\n";
+            this.drama.Visible = false;
+            // 
+            // mystery
+            // 
+            this.mystery.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.mystery.ForeColor = System.Drawing.Color.White;
+            this.mystery.Location = new System.Drawing.Point(32, 441);
+            this.mystery.Name = "mystery";
+            this.mystery.Size = new System.Drawing.Size(128, 38);
+            this.mystery.TabIndex = 26;
+            this.mystery.Text = "Mystery\r\n";
+            this.mystery.Visible = false;
+            // 
+            // western
+            // 
+            this.western.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.western.ForeColor = System.Drawing.Color.White;
+            this.western.Location = new System.Drawing.Point(32, 479);
+            this.western.Name = "western";
+            this.western.Size = new System.Drawing.Size(128, 38);
+            this.western.TabIndex = 27;
+            this.western.Text = "Western";
+            this.western.Visible = false;
+            // 
+            // scifi
+            // 
+            this.scifi.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.scifi.ForeColor = System.Drawing.Color.White;
+            this.scifi.Location = new System.Drawing.Point(32, 517);
+            this.scifi.Name = "scifi";
+            this.scifi.Size = new System.Drawing.Size(128, 38);
+            this.scifi.TabIndex = 28;
+            this.scifi.Text = "Sci-Fi";
+            this.scifi.Visible = false;
             // 
             // MainPageUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Controls.Add(this.scifi);
+            this.Controls.Add(this.western);
+            this.Controls.Add(this.mystery);
+            this.Controls.Add(this.drama);
+            this.Controls.Add(this.fantasy);
+            this.Controls.Add(this.horror);
+            this.Controls.Add(this.documentary);
+            this.Controls.Add(this.action);
+            this.Controls.Add(this.crime);
+            this.Controls.Add(this.romance);
+            this.Controls.Add(this.comedy);
+            this.Controls.Add(this.thriller);
+            this.Controls.Add(this.categoryTitle);
+            this.Controls.Add(this.categoriesBox);
             this.Controls.Add(this.menu);
             this.Controls.Add(this.pictureBox12);
             this.Controls.Add(this.pictureBox11);
@@ -223,19 +405,42 @@ namespace filmhub
             this.Size = new System.Drawing.Size(1264, 581);
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox7)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox8)).EndInit();
-            ((System.ComponentModel.ISupportInitialize) (this.pictureBox9)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox10)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox11)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox12)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.menu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.categoriesBox)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label scifi;
+
+        private System.Windows.Forms.Label mystery;
+        private System.Windows.Forms.Label western;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label comedy;
+        private System.Windows.Forms.Label romance;
+        private System.Windows.Forms.Label crime;
+        private System.Windows.Forms.Label action;
+        private System.Windows.Forms.Label documentary;
+        private System.Windows.Forms.Label horror;
+        private System.Windows.Forms.Label fantasy;
+        private System.Windows.Forms.Label drama;
+
+        private System.Windows.Forms.Label thriller;
+
+        private System.Windows.Forms.Label categoryTitle;
+
+        private System.Windows.Forms.PictureBox categoriesBox;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox9;
 
         private System.Windows.Forms.PictureBox menu;
 
@@ -244,11 +449,9 @@ namespace filmhub
         private System.Windows.Forms.PictureBox pictureBox12;
         private System.Windows.Forms.PictureBox pictureBox7;
         private System.Windows.Forms.PictureBox pictureBox8;
-        private System.Windows.Forms.PictureBox pictureBox9;
 
         private System.Windows.Forms.Label comingSoon;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox6;
--- MainPageUserControl.cs
+++ MainPageUserControl.cs
@@ -20,6 +20,73 @@ namespace filmhub
         {
             BackColor = _colors.BackgroundColor;
             menu.BackColor = _colors.BackgroundColor;
+            categoriesBox.BackColor = _colors.BackgroundColor;
+            categoryTitle.ForeColor = _colors.AccentColor;
+        }
+
+        private void menu_Click(object sender, EventArgs e)
+        {
+            categoriesBox.Visible = true;
+            
+            categoryTitle.Visible = true;
+            categoryTitle.BringToFront();
+            
+            thriller.Visible = true;
+            thriller.BringToFront();
+            
+            comedy.Visible = true;
+            comedy.BringToFront();
+            
+            romance.Visible = true;
+            romance.BringToFront();
+            
+            crime.Visible = true;
+            crime.BringToFront();
+            
+            action.Visible = true;
+            action.BringToFront();
+            
+            documentary.Visible = true;
+            documentary.BringToFront();
+            
+            horror.Visible = true;
+            horror.BringToFront();
+            
+            fantasy.Visible = true;
+            fantasy.BringToFront();
+            
+            drama.Visible = true;
+            drama.BringToFront();
+            
+            mystery.Visible = true;
+            mystery.BringToFront();
+            
+            western.Visible = true;
+            western.BringToFront();
+            
+            scifi.Visible = true;
+            scifi.BringToFront();
+            //throw new System.NotImplementedException();
+
+        }
+
+        private void categoriesBox_Click(object sender, EventArgs e)
+        {
+            categoriesBox.Visible = false;
+            categoryTitle.Visible = false;
+            thriller.Visible = false;
+            comedy.Visible = false;
+            romance.Visible = false;
+            crime.Visible = false;
+            action.Visible = false;
+            documentary.Visible = false;
+            horror.Visible = false;
+            fantasy.Visible = false;
+            drama.Visible = false;
+            mystery.Visible = false;
+            western.Visible = false;
+            scifi.Visible = false;
+            //throw new System.NotImplementedException();
         }
     }
 }
