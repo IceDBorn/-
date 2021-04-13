3f8718290108b6e80fdc15d53e93fd369fa9ca42
IceDBorn <fundwork@tutanota.com>
2021-04-13 23:32:15 +0300

    Add settings page

--- MainForm.cs
+++ MainForm.cs
@@ -35,7 +35,7 @@ namespace filmhub
         private void UserControlSelector()
         {
             mainPage.Controls.Clear();
-            mainPage.Controls.Add(new SignUpUserControl());
+            mainPage.Controls.Add(new SettingsUserControl());
         }
 
         #endregion
