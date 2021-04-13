9608887f00a0073f1ed4a7f1977adf10bc44d7b3
Olga-Panagiotidou <77547348+Olga-Panagiotidou@users.noreply.github.com>
2021-04-13 23:29:29 +0300

    user drop down

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -34,11 +34,18 @@
             this.searchButton = new System.Windows.Forms.PictureBox();
             this.accountButton = new System.Windows.Forms.PictureBox();
             this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.dropDown = new System.Windows.Forms.PictureBox();
             this.mainPage = new System.Windows.Forms.UserControl();
+            this.faves = new System.Windows.Forms.Label();
+            this.watchlist = new System.Windows.Forms.Label();
+            this.history = new System.Windows.Forms.Label();
+            this.settings = new System.Windows.Forms.Label();
+            this.logout = new System.Windows.Forms.Label();
             this.navBar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize) (this.searchButton)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.accountButton)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.logoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.dropDown)).BeginInit();
             this.SuspendLayout();
             // 
             // navBar
@@ -84,6 +91,7 @@
             this.accountButton.Size = new System.Drawing.Size(43, 43);
             this.accountButton.TabIndex = 1;
             this.accountButton.TabStop = false;
+            this.accountButton.Click += new System.EventHandler(this.accountButton_Click);
             // 
             // logoPictureBox
             // 
@@ -94,6 +102,17 @@
             this.logoPictureBox.TabIndex = 0;
             this.logoPictureBox.TabStop = false;
             // 
+            // dropDown
+            // 
+            this.dropDown.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.dropDown.Location = new System.Drawing.Point(1130, 70);
+            this.dropDown.Name = "dropDown";
+            this.dropDown.Size = new System.Drawing.Size(112, 175);
+            this.dropDown.TabIndex = 4;
+            this.dropDown.TabStop = false;
+            this.dropDown.Visible = false;
+            this.dropDown.Click += new System.EventHandler(this.dropDown_Click);
+            // 
             // mainPage
             // 
             this.mainPage.BackColor = System.Drawing.SystemColors.ActiveCaption;
@@ -102,11 +121,74 @@
             this.mainPage.Size = new System.Drawing.Size(1264, 581);
             this.mainPage.TabIndex = 1;
             // 
+            // faves
+            // 
+            this.faves.BackColor = System.Drawing.Color.Transparent;
+            this.faves.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.faves.ForeColor = System.Drawing.Color.White;
+            this.faves.Location = new System.Drawing.Point(1143, 81);
+            this.faves.Name = "faves";
+            this.faves.Size = new System.Drawing.Size(89, 31);
+            this.faves.TabIndex = 5;
+            this.faves.Text = "Favorites";
+            this.faves.Visible = false;
+            this.faves.Click += new System.EventHandler(this.faves_Click);
+            // 
+            // watchlist
+            // 
+            this.watchlist.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.watchlist.ForeColor = System.Drawing.Color.White;
+            this.watchlist.Location = new System.Drawing.Point(1143, 112);
+            this.watchlist.Name = "watchlist";
+            this.watchlist.Size = new System.Drawing.Size(89, 33);
+            this.watchlist.TabIndex = 6;
+            this.watchlist.Text = "Watchlist";
+            this.watchlist.Visible = false;
+            // 
+            // history
+            // 
+            this.history.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.history.ForeColor = System.Drawing.Color.White;
+            this.history.Location = new System.Drawing.Point(1143, 145);
+            this.history.Name = "history";
+            this.history.Size = new System.Drawing.Size(89, 33);
+            this.history.TabIndex = 7;
+            this.history.Text = "History";
+            this.history.Visible = false;
+            // 
+            // settings
+            // 
+            this.settings.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.settings.ForeColor = System.Drawing.Color.White;
+            this.settings.Location = new System.Drawing.Point(1143, 178);
+            this.settings.Name = "settings";
+            this.settings.Size = new System.Drawing.Size(89, 28);
+            this.settings.TabIndex = 8;
+            this.settings.Text = "Settings";
+            this.settings.Visible = false;
+            // 
+            // logout
+            // 
+            this.logout.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.logout.ForeColor = System.Drawing.Color.White;
+            this.logout.Location = new System.Drawing.Point(1143, 206);
+            this.logout.Name = "logout";
+            this.logout.Size = new System.Drawing.Size(89, 32);
+            this.logout.TabIndex = 9;
+            this.logout.Text = "Log out";
+            this.logout.Visible = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.logout);
+            this.Controls.Add(this.settings);
+            this.Controls.Add(this.history);
+            this.Controls.Add(this.watchlist);
+            this.Controls.Add(this.faves);
+            this.Controls.Add(this.dropDown);
             this.Controls.Add(this.mainPage);
             this.Controls.Add(this.navBar);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
@@ -120,8 +202,18 @@
             ((System.ComponentModel.ISupportInitialize) (this.searchButton)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.accountButton)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.logoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.dropDown)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label watchlist;
+        private System.Windows.Forms.Label history;
+        private System.Windows.Forms.Label settings;
+        private System.Windows.Forms.Label logout;
+
+        private System.Windows.Forms.Label faves;
+
+        private System.Windows.Forms.PictureBox dropDown;
 
         private filmhub.RoundedTextBox searchBar;
 
--- MainForm.cs
+++ MainForm.cs
@@ -4,7 +4,9 @@ using System.Windows.Forms;
 namespace filmhub
 {
     public partial class MainForm : Form
-    {
+    { 
+        private FavoritesUserControl favesPage = new FavoritesUserControl();
+        
         #region Fields
         
         private readonly Colors _colors = new Colors();
@@ -30,6 +32,12 @@ namespace filmhub
             searchBar.BackColor = _colors.FieldColor;
             searchBar.ForeColor = _colors.FieldDarkTextColor;
             mainPage.BackColor = _colors.BackgroundColor;
+            dropDown.BackColor = _colors.BackgroundColor;
+            faves.BackColor = _colors.BackgroundColor;
+            watchlist.BackColor = _colors.BackgroundColor;
+            history.BackColor = _colors.BackgroundColor;
+            settings.BackColor = _colors.BackgroundColor;
+            logout.BackColor = _colors.BackgroundColor;
         }
 
         private void UserControlSelector()
@@ -56,6 +64,53 @@ namespace filmhub
             {
                 searchBar.Text = @" Enter your search";
             }
+        }
+
+        private void dropDown_Click(object sender, EventArgs e)
+        {
+            dropDown.BringToFront();
+            //throw new System.NotImplementedException();
+        }
+        
+        int count = 0;
+        private void accountButton_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                dropDown.Visible = true;
+
+                faves.Visible = true;
+                faves.BringToFront();
+
+                watchlist.Visible = true;
+                watchlist.BringToFront();
+
+                history.Visible = true;
+                history.BringToFront();
+
+                settings.Visible = true;
+                settings.BringToFront();
+
+                logout.Visible = true;
+                logout.BringToFront();
+                //throw new System.NotImplementedException();
+                count = count + 1;
+            }else if (count == 1)
+            {
+                dropDown.Visible = false;
+                faves.Visible = false;
+                watchlist.Visible = false;
+                history.Visible = false;
+                settings.Visible = false;
+                logout.Visible = false;
+                //throw new System.NotImplementedException();
+                count = 0;
+            }
+        }
+
+        private void faves_Click(object sender, EventArgs e)
+        {
+            //throw new System.NotImplementedException();
         }
     }
 }
