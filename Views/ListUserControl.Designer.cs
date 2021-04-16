b71ce27e42cac5f727b715eeeb52a4631a004759
Olga-Panagiotidou <77547348+Olga-Panagiotidou@users.noreply.github.com>
2021-04-16 22:38:51 +0300

    add designer files

---
+++ Views/ListUserControl.Designer.cs
@@ -0,0 +1,130 @@
+﻿using System.ComponentModel;
+
+namespace filmhub.Views
+{
+    partial class ListUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.window = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.image = new System.Windows.Forms.ColumnHeader();
+            this.text = new System.Windows.Forms.ColumnHeader();
+            this.button = new System.Windows.Forms.ColumnHeader();
+            this.menu = new System.Windows.Forms.PictureBox();
+            this.categoriesPanel = new System.Windows.Forms.UserControl();
+            ((System.ComponentModel.ISupportInitialize) (this.menu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // window
+            // 
+            this.window.Font = new System.Drawing.Font("Segoe UI", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.window.ForeColor = System.Drawing.Color.White;
+            this.window.Location = new System.Drawing.Point(75, 22);
+            this.window.Name = "window";
+            this.window.Size = new System.Drawing.Size(911, 49);
+            this.window.TabIndex = 0;
+            this.window.Text = "Window\r\n\r\n";
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {this.image, this.text, this.button});
+            this.listView1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (161)));
+            this.listView1.Location = new System.Drawing.Point(75, 114);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1122, 404);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // image
+            // 
+            this.image.Text = "Image";
+            this.image.Width = 365;
+            // 
+            // text
+            // 
+            this.text.Text = "Text";
+            this.text.Width = 394;
+            // 
+            // button
+            // 
+            this.button.Text = "Button";
+            this.button.Width = 356;
+            // 
+            // menu
+            // 
+            this.menu.Image = global::filmhub.Properties.Resources.menu;
+            this.menu.Location = new System.Drawing.Point(17, 22);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(52, 49);
+            this.menu.TabIndex = 2;
+            this.menu.TabStop = false;
+            this.menu.Visible = false;
+            this.menu.Click += new System.EventHandler(this.menu_Click);
+            this.menu.MouseLeave += new System.EventHandler(this.menu_MouseLeave);
+            this.menu.MouseHover += new System.EventHandler(this.menu_MouseHover);
+            // 
+            // categoriesPanel
+            // 
+            this.categoriesPanel.Location = new System.Drawing.Point(0, 0);
+            this.categoriesPanel.Name = "categoriesPanel";
+            this.categoriesPanel.Size = new System.Drawing.Size(248, 592);
+            this.categoriesPanel.TabIndex = 3;
+            this.categoriesPanel.Visible = false;
+            // 
+            // ListUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Controls.Add(this.categoriesPanel);
+            this.Controls.Add(this.menu);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.window);
+            this.Name = "ListUserControl";
+            this.Size = new System.Drawing.Size(1264, 595);
+            ((System.ComponentModel.ISupportInitialize) (this.menu)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.UserControl categoriesPanel;
+
+        private System.Windows.Forms.PictureBox menu;
+
+        private System.Windows.Forms.ColumnHeader button;
+        private System.Windows.Forms.ColumnHeader image;
+        private System.Windows.Forms.ColumnHeader text;
+
+        private System.Windows.Forms.Label window;
+        private System.Windows.Forms.ListView listView1;
+
+        #endregion
+    }
+}
