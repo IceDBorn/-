9ff3e32450b6190b9d58a6e99b490a6889fbd5f2
CrazyStevenz <spapanatsios@gmail.com>
2021-04-16 23:18:18 +0200

    Remove birthdate from Account model

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -13,19 +13,19 @@ namespace filmhub
             var id = -1;
             string name = null, surname = null, picture = null;
             bool admin = false, darkTheme = true;
-            var birthdate = NpgsqlDate.Infinity;
             var createdOn = NpgsqlDateTime.Infinity;
-            
-            try {
+
+            try
+            {
                 var con = DatabaseController.getConnection();
                 con.Open();
 
-                var validateUser = 
-                    "SELECT id, name, surname, birthdate, admin, dark_theme, picture, created_on " +
+                var validateUser =
+                    "SELECT id, name, surname, admin, dark_theme, picture, created_on " +
                     "FROM account " +
                     "WHERE email = '" + email + "' AND password = '" + password + "' " +
                     "LIMIT 1";
-                
+
                 using var cmd = new NpgsqlCommand(validateUser, con);
 
                 using var rdr = cmd.ExecuteReader();
@@ -34,24 +34,30 @@ namespace filmhub
                     id = rdr.GetInt32(0);
                     name = rdr.GetString(1);
                     surname = rdr.GetString(2);
-                    birthdate = rdr.GetDate(3);
-                    admin = rdr.GetBoolean(4);
-                    darkTheme = rdr.GetBoolean(5);
-                    // picture = rdr.GetString(6); TODO: Fix
-                    createdOn = rdr.GetTimeStamp(7);
+                    admin = rdr.GetBoolean(3);
+                    darkTheme = rdr.GetBoolean(4);
+                    // picture = rdr.GetString(5); TODO: Fix
+                    createdOn = rdr.GetTimeStamp(6);
                 }
-            } catch (Exception e)
+            }
+            catch (Exception e)
             {
                 MessageBox.Show(e.Message);
             }
-    
-            if (name != null && id != -1) {
-                if (admin) {
+
+            if (name != null && id != -1)
+            {
+                if (admin)
+                {
                     // TODO: Admin account
-                } else {
-                    new Account(id, name, surname, email, birthdate, admin, darkTheme, picture, createdOn).login();
                 }
-            } else {
+                else
+                {
+                    new Account(id, name, surname, email, admin, darkTheme, picture, createdOn).login();
+                }
+            }
+            else
+            {
                 MessageBox.Show("Wrong username or password.");
             }
         }
