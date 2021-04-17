fb0d9c1d2c7fc0fec22ab388cd935ce3322d2b62
CrazyStevenz <spapanatsios@gmail.com>
2021-04-17 17:13:06 +0200

    Rename SearchEngine to SearchController - Fix not escaping characters

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Documents;
@@ -13,121 +13,104 @@ using Npgsql;
 
 namespace filmhub.Controllers
 {
-    public class SearchEngine
+    public class SearchController
     {
-        private static string _path;    
-        
-        public SearchEngine(string path)
+        private static string _path;
+
+        public SearchController(string path)
         {
-            //path is the folder for index files
+            // Path is the folder for index files
             _path = path;
         }
-        
+
         private static IndexWriter CreateWriter()
         {
-            FSDirectory dir = FSDirectory.Open(_path);
-            IndexWriterConfig config = new IndexWriterConfig(LuceneVersion.LUCENE_48,new StandardAnalyzer(LuceneVersion.LUCENE_48));
-            IndexWriter writer = new IndexWriter(dir, config);
+            var dir = FSDirectory.Open(_path);
+            var config = new IndexWriterConfig(LuceneVersion.LUCENE_48, new StandardAnalyzer(LuceneVersion.LUCENE_48));
+            var writer = new IndexWriter(dir, config);
             return writer;
         }
         
-        
-        //Create entity for indexing. ID=movie id in database, text=text to be scanned on user search (title+" "+stars"+" "+...)
-        private static Document CreateDocument(int id, string text) 
+        private static Document CreateDocument(int id, string text)
         {
-            Document document = new Document();
-            document.Add(new StringField("id", id.ToString() , Field.Store.YES));
-            document.Add(new TextField("text", text , Field.Store.YES));
+            var document = new Document
+            {
+                new StringField("id", id.ToString(), Field.Store.YES),
+                new TextField("text", text, Field.Store.YES)
+            };
             return document;
         }
-        
-        public IndexSearcher CreateSearcher()
+
+        private static IndexSearcher CreateSearcher()
         {
             Directory dir = FSDirectory.Open(_path);
             IndexReader reader = DirectoryReader.Open(dir);
-            IndexSearcher searcher = new IndexSearcher(reader);
+            var searcher = new IndexSearcher(reader);
             return searcher;
         }
-        
-        public TopDocs Search(string text, IndexSearcher searcher)
+
+        private static TopDocs Search(string text, IndexSearcher searcher)
         {
-            QueryParser qp = new QueryParser(LuceneVersion.LUCENE_40, "text",
+            var qp = new QueryParser(LuceneVersion.LUCENE_40, "text",
                 new StandardAnalyzer(LuceneVersion.LUCENE_40));
-            Query idQuery = qp.Parse(text);
-            TopDocs hits = searcher.Search(idQuery,10);
+            var escapedString = QueryParserBase.Escape(text);
+            var idQuery = qp.Parse(escapedString);
+            var hits = searcher.Search(idQuery, 10);
             return hits;
         }
-        
-        private static void Indexer(List<Document> list)
+
+        private static void Indexer(IEnumerable<Document> list)
         {
-            IndexWriter writer = CreateWriter();
+            var writer = CreateWriter();
             writer.AddDocuments(list);
             writer.Commit();
             writer.Dispose();
         }
 
-        public List<int> SearchIdResults(string searchQuery)
+        public static IEnumerable<int> SearchIdResults(string searchQuery)
         {
-            IndexSearcher searcher = CreateSearcher();
-            TopDocs foundDocs = Search(searchQuery, searcher);
-            
+            var searcher = CreateSearcher();
+            var foundDocs = Search(searchQuery, searcher);
+
             //FOR TESTING:
             Console.WriteLine("Total Results :: " + foundDocs.TotalHits);
 
-            List<int> results = new List<int>();
-            foreach (ScoreDoc sd in foundDocs.ScoreDocs)
+            var results = new List<int>();
+            foreach (var sd in foundDocs.ScoreDocs)
             {
-                results.Add(Int32.Parse(searcher.Doc(sd.Doc).Get("id")));
+                results.Add(int.Parse(searcher.Doc(sd.Doc).Get("id")));
             }
+
             return results;
         }
 
         public static void createIndex()
         {
-            try {
+            try
+            {
                 var con = DatabaseController.getConnection();
                 con.Open();
 
-                var movieData = 
-                    "SELECT id,name " +
-                    "FROM movie ";
-                
+                const string movieData = "SELECT id,name " +
+                                         "FROM movie ";
+
                 using var cmd = new NpgsqlCommand(movieData, con);
 
                 using var rdr = cmd.ExecuteReader();
-                
-                List<Document> list= new List<Document>();
-                
+
+                var list = new List<Document>();
+
                 while (rdr.Read())
                 {
-                    list.Add(CreateDocument(rdr.GetInt32(0),rdr.GetString(1)));
-                    
+                    list.Add(CreateDocument(rdr.GetInt32(0), rdr.GetString(1)));
                 }
+
                 Indexer(list);
-            } catch (Exception e)
+            }
+            catch (Exception e)
             {
                 MessageBox.Show(e.Message);
             }
         }
-        
-        
-        /*
-        EXAMPLE INDEXING
-        Document d1 = CreateDocument(1,"Shrek Animation Commedy Actorname1 Actorname2 ...");
-        List<Document> list= new List<Document>();
-        list.Add(d1);
-        Indexer(list);
-        */
-        
-        /*
-        EXAMPLE SEARCH
-        searchQuery= "Comedy 2019 2020";
-        List<int> list = SearchIdResults(searchQuery);
-        foreach(int i in list){
-            //pull content from database with id number i
-        }
-         
-        */
-        
     }
 }
