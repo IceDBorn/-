69e5fd45f9d011ee0ec33e17efeda69c7596fffc
CrazyStevenz <spapanatsios@gmail.com>
2021-04-27 16:45:48 +0200

    Move main page movie fetching code to MovieController

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using filmhub.Models;
+using Npgsql;
+
+namespace filmhub.Controllers
+{
+    public static class MovieController
+    {
+        private static readonly NpgsqlConnection con = DatabaseController.GetConnection();
+
+        public enum Section
+        {
+            Featured,
+            ComingSoon
+        }
+
+        public static List<Movie> MainPageMovieQuery(Section section)
+        {
+            var movies = new List<Movie>();
+            var query = "SELECT id, picture FROM movie WHERE release_date ";
+            
+            if (section == Section.Featured)
+            {
+                query += "<";
+            }
+            else
+            {
+                query += ">";
+            }
+            
+            query += " NOW() ORDER BY RANDOM() LIMIT 5";
+
+            try
+            {
+                using var cmd = new NpgsqlCommand(query, con);
+                using var rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var movie = new Movie {Id = rdr.GetInt32(0), Picture = rdr.GetString(1)};
+                    movies.Add(movie);
+                }
+
+                rdr.Close();
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            catch
+            {
+                MessageBox.Show(@"Something went wrong while contacting the database.");
+            }
+
+            return movies;
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace filmhub.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Picture { get; set; }
+    }
+}
