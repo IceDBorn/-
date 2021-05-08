3ad65007da91c66391b4a5b9ae0bf38f260716a0
IceDBorn <fundwork@tutanota.com>
2021-05-08 23:09:28 +0300

    Fix auto login error

--- Views/MainForm.cs
+++ Views/MainForm.cs
@@ -131,14 +131,12 @@ namespace filmhub.Views
 
         private async void MainForm_Load(object sender, EventArgs e)
         {
-            if (Settings.Default.RememberMe)
-            {
-                AccountController.Login(Settings.Default.Username, Settings.Default.Password);
-                if (Account.GetAccountInstance() == null) return;
-                Program.MainForm.UserControlSelector(new MainPageUserControl(), true);
-            }
             SearchController.RemoveIndex();
             await SearchController.CreateIndexAsync();
+            if (!Settings.Default.RememberMe) return;
+            AccountController.Login(Settings.Default.Username, Settings.Default.Password);
+            if (Account.GetAccountInstance() == null) return;
+            Program.MainForm.UserControlSelector(new MainPageUserControl(), true);
         }
 
         private void searchBar_Enter(object sender, EventArgs e)
